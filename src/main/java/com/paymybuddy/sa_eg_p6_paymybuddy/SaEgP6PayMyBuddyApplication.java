8700b2f454d89b91865cbdfa3dd646fc2630bc32
TheGuiff <ergu@mac.com>
2022-09-29 11:06:14 +0200

    feat:creation

---
+++ src/main/java/com/paymybuddy/sa_eg_p6_paymybuddy/SaEgP6PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.sa_eg_p6_paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaEgP6PayMyBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaEgP6PayMyBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymybuddy/sa_eg_p6_paymybuddy/SaEgP6PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.sa_eg_p6_paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaEgP6PayMyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
