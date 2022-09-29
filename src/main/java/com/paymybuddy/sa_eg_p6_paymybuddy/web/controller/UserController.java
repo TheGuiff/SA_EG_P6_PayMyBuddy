86ed93399ed1e7f80894c2fef2322998118466f7
TheGuiff <ergu@mac.com>
2022-09-29 11:48:43 +0200

    feat:web_first_controller

--- src/main/java/com/paymybuddy/sa_eg_p6_paymybuddy/web/controller/UserController.java
+++ src/main/java/com/paymybuddy/sa_eg_p6_paymybuddy/web/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.paymybuddy.sa_eg_p6_paymybuddy.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
 }
